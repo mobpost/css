ccf1635539c49aa4b90384296e2d90671ffdfead
mobpost <asepmulyana.ht1@gmail.com>
2016-10-27 00:09:41 +0700

    Create blog-pager.js

---
+++ blog-pager.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){var aa=$('#links').val();if(aa == null){window.location.href = 'http://chepps.blogspot.com/';};$('#footer').attr('href','http://chepps.blogspot.com/');});
+$(document).ready(function(){$('.footer-section').load('https://cdn.rawgit.com/mobpost/css/html/blogger.html');});
